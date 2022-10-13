6bed8c11ee498643bb4c30384ac289ae917a62b1
laithmahdi <leon-mahdi@mail.ru>
2022-10-13 22:15:40 +0300

    Добавил папку с ДЗ к по первому семинару C#

---
+++ DZ_1/Max_3/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.WriteLine("Введите первое число");
+int number_1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+int number_2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите третье число");
+int number_3 = Convert.ToInt32(Console.ReadLine());
+
+int max = number_1;
+
+if (number_2>max)
+{
+    max=number_2;
+}
+if (number_3>max)
+{
+    max=number_3;
+}
+Console.Write("Максимальное число ");
+Console.WriteLine(max);
