db055086994fb720679d5224868ab3e1b92161f1
laithmahdi <leon-mahdi@mail.ru>
2022-10-14 08:49:55 +0300

    Написал чётные от 1 до N в DZ_1

---
+++ DZ_1/ChetnieOt_1_do_N/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.WriteLine("Введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+int chet = 2;
+while (chet<=N)
+{
+    Console.WriteLine(chet);
+    chet=chet+2;
+}
