da802dedf27949ca2b1a9e594fa97afea358df34
laithmahdi <leon-mahdi@mail.ru>
2022-10-21 16:53:57 +0300

    Написал задачу с кубами чисел

---
+++ DZ_3/Cubes/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.WriteLine("Введите число");
+int N = int.Parse(Console.ReadLine());
+int number = 1;
+double cube = 0;
+while (number<=N)
+{
+    cube = Math.Pow(number,3);
+    Console.WriteLine(cube);
+    number++;
+}
