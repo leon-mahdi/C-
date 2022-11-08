5b06146fc9654012278f239c4f24f711995dcb83
laithmahdi <leon-mahdi@mail.ru>
2022-11-08 20:44:01 +0300

    Написал задачу со степенями

---
+++ DZ_4/A_v_stepen_B/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16
+
+Console.WriteLine("Введите число А");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число B");
+int b = int.Parse(Console.ReadLine());
+
+
+double x = 0;
+double Func (double sum)
+{
+
+    double x = Math.Pow(a,b);
+    return x;
+    
+}
+
+Console.WriteLine(Func(a));
+
