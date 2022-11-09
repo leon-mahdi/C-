3999dc6ae7c31c8a52b098064db762f30d3c28ac
laithmahdi <leon-mahdi@mail.ru>
2022-11-09 18:50:10 +0300

    Массив на 8 случайных чисел до сотни

---
+++ DZ_4/Array8_random/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+// 6, 1, 33 -> [6, 1, 33]
+
+
+
+int[]array = GetArray (8);
+
+int [] GetArray (int size)
+{
+    int[]array = new int [8];
+    for(int i=0; i<size; i++)
+    {
+        array[i] = new Random().Next(0,101);
+        
+    }
+    return array;
+}
+
+Console.WriteLine("[" + String.Join(",", array) + "]");
