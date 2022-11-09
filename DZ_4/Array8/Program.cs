d94e5cd412bd9637a12ae9ae6c44202637b905a7
laithmahdi <leon-mahdi@mail.ru>
2022-11-09 18:46:35 +0300

    Элементы массива от пользователя

---
+++ DZ_4/Array8/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+// 6, 1, 33 -> [6, 1, 33]
+
+
+
+
+// Этот вариант будет принимать элементы от пользователя
+
+
+
+
+int[]array = GetArray (8);
+
+int [] GetArray (int size)
+{
+    int[]array = new int [8];
+
+    Console.WriteLine("Поочерёдно введите элементы массива");
+
+    for(int i=0; i<8; i++)
+    {
+        array[i] = int.Parse(Console.ReadLine());
+    }
+
+    return array;
+
+}
+
+Console.WriteLine("[" + String.Join(",", array)+ "]");
+
+
