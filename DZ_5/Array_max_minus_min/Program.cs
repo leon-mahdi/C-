7307973efff1b42420ed57fb74cf0176e07e67e5
laithmahdi <leon-mahdi@mail.ru>
2022-11-12 19:07:23 +0300

    Исправил задачу 38

--- DZ_5/Array_max_minus_min/Program.cs
+++ DZ_5/Array_max_minus_min/Program.cs
@@ -9,26 +9,33 @@ int size = int.Parse(Console.ReadLine());
 
 int [] array = new int [size];
 
-int min = 0;
 int max = 0;
-
+int min= 0;
 
 for (int i =0; i<size; i++)
 {
     array[i] = new Random().Next(100);
-    min=array[1];
-    if(array[i]>=max)
+
+    min= array[0];
+    max = array[0];
+
+    Console.Write(array[i] + ", ");  
+}
+
+for (int i =0; i<size; i++)
+{
+    if(array[i]>max)
     {
         max = array[i];
     }
-    else
+
+    if (array[i]<min)
     {
         min = array[i];
     }
-    
-    Console.Write(array[i] + ", ");
-    
-}
+}     
+
 int result = max - min;
+
 Console.WriteLine();
 Console.WriteLine("Разница между максимальным и минимальным элементов массива составляет " + result);
