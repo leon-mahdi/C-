468fff5b0b56d8e397ed740d30cab49ccadfce5f
laithmahdi <leon-mahdi@mail.ru>
2022-11-12 18:16:42 +0300

    Решил задачу на сумму эл с нечетной позицией

---
+++ DZ_5/Array_summa_nechet_elem/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+
+// [3, 7, 23, 12] -> 19
+
+// [-4, -6, 89, 6] -> 0
+
+Console.WriteLine("Введите количество элементов в массиве ");
+
+int size = Convert.ToInt32(Console.ReadLine());
+
+int [] array = new int [size];
+
+int sum = 0;
+
+for (int i =0; i<size; i++)
+{
+    array[i]= new Random().Next(-100,101);
+    if (i%2>0)
+    {
+        sum+=array[i];
+    }
+
+    Console.Write(array[i] + ", ");
+}
+
+Console.WriteLine();
+Console.WriteLine("Сумма нечётных элементов массива равна " + sum);
