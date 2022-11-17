b8fbb7c1972c888bd44784062a20d262b6590f1b
laithmahdi <leon-mahdi@mail.ru>
2022-11-17 22:50:29 +0300

    Дополнительный вариант задачи с отр числами

---
+++ DZ_6/Array_0+_dop/Program.cs
@@ -0,0 +1,36 @@
+﻿// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 3
+
+
+// Дополнительно вариант, где пользователь задает только количество чисел, а сами числа случайны
+
+
+
+Console.WriteLine("Введите количество чисел");
+int M = Convert.ToInt32(Console.ReadLine());
+
+int count=0;
+
+int [] array = GetArray(M);
+
+int [] GetArray(int M)
+{
+    int [] array = new int [M];
+    for (int i =0; i<M; i++)
+    {
+        array[i] = new Random().Next(-1000,1001);
+        if (array[i] < 0)
+        {
+            count++;
+        }
+    }
+    return array;
+}
+
+Console.WriteLine("[" + String.Join(",", array)+"]");
+
+Console.WriteLine("Количество чисел менее 0: " + count);
+
