72cf9486164829c518ee5775bc65465c6fa6f157
laithmahdi <leon-mahdi@mail.ru>
2022-11-17 22:44:27 +0300

    Решил задачу с количеством отр чисел

---
+++ DZ_6/Array_0+verno/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 3
+
+Console.WriteLine("Введите количество чисел");
+int M = Convert.ToInt32(Console.ReadLine());
+
+int count =0;
+
+for (int i =0; i<M; i++)
+{
+    Console.WriteLine("Введите число");
+    int number = Convert.ToInt32(Console.ReadLine());
+    if (number<0)
+    {
+        count++;
+    }
+}
+
+Console.WriteLine("Количество чисел менее 0: " + count);
