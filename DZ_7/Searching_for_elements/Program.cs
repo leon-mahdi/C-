f7738361e5864c437968c0a302d2f66324537c3a
laithmahdi <leon-mahdi@mail.ru>
2022-11-22 19:28:14 +0300

    Решил задачу на поиск элемента по позиции

---
+++ DZ_7/Searching_for_elements/Program.cs
@@ -0,0 +1,35 @@
+﻿// Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+// и возвращает значение этого элемента или же указание, 
+// что такого элемента нет.
+
+Console.WriteLine("Введите количество строк ");
+int rows = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите количество столбцов ");
+int columns = int.Parse(Console.ReadLine());
+
+int[,] matrix = new int[rows, columns];
+
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < columns; j++)
+    {
+        matrix[i, j] = new Random().Next(11);
+        Console.Write(matrix[i, j] + "\t");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+Console.WriteLine("Введите строку искомого элемента");
+int x = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите столбец искомого элемента");
+int y = int.Parse(Console.ReadLine());
+if (x > rows || y > columns)
+{
+    Console.WriteLine("Позиция элемента указана неверно. Элемента с такой позицией нет.");
+}
+else
+{
+    Console.WriteLine("Элемент с искомой позицией равен " + matrix[x - 1, y - 1]);
+}
