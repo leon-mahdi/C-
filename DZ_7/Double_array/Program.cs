59d25348fc92a1fdf2d445f232468c42f3ac1c08
laithmahdi <leon-mahdi@mail.ru>
2022-11-22 17:30:28 +0300

    Написал задачу на рандомный двумерноый массив

---
+++ DZ_7/Double_array/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+Console.WriteLine("Введите количество строк ");
+int rows = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите количество столбцов ");
+int columns = int.Parse(Console.ReadLine());
+
+
+int [,] matrix = new int [rows,columns];
+
+for (int i =0; i<matrix.GetLength(0); i++) 
+{
+    for (int j =0; j<matrix.GetLength(1); j++)
+    {
+        matrix [i,j] = new Random().Next(101);
+        Console.Write(matrix [i,j] + "\t");
+    }
+    Console.WriteLine();
+}
