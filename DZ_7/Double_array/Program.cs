85c4a12b1fb3a0ba75381052f58e7d914155926e
laithmahdi <leon-mahdi@mail.ru>
2022-11-23 07:21:02 +0300

    Исправил вещественные числа

--- DZ_7/Double_array/Program.cs
+++ DZ_7/Double_array/Program.cs
@@ -7,13 +7,14 @@ Console.WriteLine("Введите количество столбцов ");
 int columns = int.Parse(Console.ReadLine());
 
 
-int [,] matrix = new int [rows,columns];
+double [,] matrix = new double [rows,columns];
 
 for (int i =0; i<matrix.GetLength(0); i++) 
 {
     for (int j =0; j<matrix.GetLength(1); j++)
     {
-        matrix [i,j] = new Random().Next(101);
+        matrix [i,j] = new Random().Next(-10,11)+ new Random().NextDouble();
+        matrix[i,j] = Math.Round(matrix[i,j],1);
         Console.Write(matrix [i,j] + "\t");
     }
     Console.WriteLine();
