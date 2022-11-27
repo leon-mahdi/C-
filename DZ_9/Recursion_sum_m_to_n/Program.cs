dfaf152b51395208c90cc6def1c481a24712f02d
laithmahdi <leon-mahdi@mail.ru>
2022-11-27 21:47:53 +0300

    Решил сумму от M до N

---
+++ DZ_9/Recursion_sum_m_to_n/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.WriteLine("Введите начальное число");
+int M = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите конечное число");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int PrintSum(int start, int end)
+{
+
+    if (start == end)
+    {
+        return end;
+    }
+    int sum = start + PrintSum(start + 1, end);
+    return sum;
+
+}
+
+Console.WriteLine(PrintSum(M, N));
