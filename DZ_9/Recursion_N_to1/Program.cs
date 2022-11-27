c6720bfc2350b0606358dc49f5713c26ee84b4ba
laithmahdi <leon-mahdi@mail.ru>
2022-11-27 20:39:22 +0300

    Решил рекрсию от N до 1

---
+++ DZ_9/Recursion_N_to1/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+
+Console.WriteLine("Введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+int end = 1;
+
+string PrintNumber(int start)
+{
+
+    if (start == end)
+    {
+        return end.ToString();
+    }
+    return (start + ", " + PrintNumber(start-1));
+
+
+
+}
+
+Console.WriteLine(PrintNumber(N));
