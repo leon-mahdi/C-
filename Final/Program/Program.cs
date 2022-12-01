004b51a138323c07665482a4419c14b54a58d04d
laithmahdi <leon-mahdi@mail.ru>
2022-12-01 16:05:56 +0300

    Написал программу для итогового проекта

--- Final/Program/Program.cs
+++ Final/Program/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.WriteLine("Введите количество элементов в массиве ");
+
+int size = int.Parse(Console.ReadLine());
+
+string[] array = new string[size];
+int newSize = 0;
+for (int i = 0; i < size; i++)
+{
+    Console.WriteLine("Введите элемент массива");
+    array[i] = Console.ReadLine();
+    if (array[i].Length <= 3)
+    {
+        newSize++;
+    }
+}
+Console.WriteLine("Первоначальный массив [" + String.Join(",", array) + "]");
+
+string[] newArray = new string[newSize];
+int j = 0;
+for (int i = 0; i < size; i++)
+{
+
+    if (array[i].Length <= 3)
+    {
+        newArray[j] = array[i];
+        j++;
+    }
+}
+
+Console.WriteLine("Итоговый массив [" + String.Join(",", newArray) + "]");
